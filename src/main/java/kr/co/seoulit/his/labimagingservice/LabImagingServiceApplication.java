af9994d79c0fb860c3d5f370529b5863bbea31a2
jinki238 <jinki238@gmail.com>
2026-07-07 11:21:06 +0900

    commit

---
+++ src/main/java/kr/co/seoulit/his/labimagingservice/LabImagingServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.seoulit.his.labimagingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabImagingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabImagingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/seoulit/his/labimagingservice/LabImagingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.seoulit.his.labimagingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabImagingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
